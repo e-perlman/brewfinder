a2d684338f20781cd8dd12b5bfddb0c898180b1f
Emma Perlman <emmajperlman@gmail.com>
2022-03-18 16:38:07 -0700

    Conditional function for form input even listener

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 function init(){
-const searchState=document.querySelector('#search-state')
-searchState.addEventListener('submit',e=>{
+const searchForm=document.querySelector('#search-form')
+searchForm.addEventListener('submit',e=>{
     e.preventDefault()
-    const inputState=document.querySelector('#input-state') 
-    console.log(inputState.value) 
+    const inputState=document.querySelector('#input-state').value
+    const inputCity=document.querySelector('#input-city').value 
+    const inputKeyword=document.querySelector('#input-keyword').value
+
+    getList(inputState,inputCity,inputKeyword)
 })
-searchState.addEventListener('submit',e=>{
-    e.preventDefault()
-    const inputCity=document.querySelector('#input-city') 
-    console.log(inputCity.value) 
-})
+function getList(state,city,keyword){
+    if (keyword.length>0){
+        console.log(keyword)
+    }
+    else if(keyword.length===0&&city.length>0){
+        console.log(city)
+    }
+    else if(keyword.length===0&&city.length==0&&state.length>0)
+        console.log(state)
+    else{
+        console.log('need more info!')
+    }
+}
 }
 document.addEventListener("DOMContentLoaded", init);
