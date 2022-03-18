4f515e4744918b26cc4dcf89e0e311647603a341
Emma Perlman <emmajperlman@gmail.com>
2022-03-18 16:52:29 -0700

    More conditions in form conditional for city/state filters

--- js/index.js
+++ js/index.js
@@ -9,12 +9,24 @@ searchForm.addEventListener('submit',e=>{
     getList(inputState,inputCity,inputKeyword)
 })
 function getList(state,city,keyword){
-    if (keyword.length>0){
+    //Order of specific search: keyword,city,state
+    // Only a keyword is inputted
+    if (keyword.length>0&&city.length===0&&state.length===0){
         console.log(keyword)
     }
+    //Keyword and city inputted (state doesn't matter), search by keyword and refine by city
+    else if(keyword.length>0&&city.length>0){
+        console.log(`${keyword} ${city}`)
+    }
+    //Only keyword and state is inputted, search by keyword and refine by state
+    else if(keyword.length>0&&city.length===0&&state.length>0){
+        console.log(`${keyword} ${state}`)
+    }
+    //Only city inputted (state doesn't matter)
     else if(keyword.length===0&&city.length>0){
         console.log(city)
     }
+    //Only state inputted
     else if(keyword.length===0&&city.length==0&&state.length>0)
         console.log(state)
     else{
