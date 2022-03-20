ed56dc38e8fa6b4c043c01a32c6f7c4b8beb07bb
Emma Perlman <emmajperlman@gmail.com>
2022-03-20 13:54:29 -0700

    add fetch functions

--- js/index.js
+++ js/index.js
@@ -6,32 +6,56 @@ searchForm.addEventListener('submit',e=>{
     const inputCity=document.querySelector('#input-city').value 
     const inputKeyword=document.querySelector('#input-keyword').value
 
-    getList(inputState,inputCity,inputKeyword)
+    searchBy(inputState,inputCity,inputKeyword)
 })
-function getList(state,city,keyword){
+function searchBy(state,city,keyword){
     //Order of specific search: keyword,city,state
     // Only a keyword is inputted
     if (keyword.length>0&&city.length===0&&state.length===0){
-        console.log(keyword)
+        fetchKeyword(keyword.toLowerCase())
     }
     //Keyword and city inputted (state doesn't matter), search by keyword and refine by city
     else if(keyword.length>0&&city.length>0){
-        console.log(`${keyword} ${city}`)
+        formatSearch(city.toLowerCase())
+        //console.log(`${keyword} ${city}`)
     }
     //Only keyword and state is inputted, search by keyword and refine by state
     else if(keyword.length>0&&city.length===0&&state.length>0){
-        console.log(`${keyword} ${state}`)
+        formatSearch(state.toLowerCase())
+        //console.log(`${keyword} ${state}`)
     }
     //Only city inputted (state doesn't matter)
     else if(keyword.length===0&&city.length>0){
-        console.log(city)
+        formatSearch(city.toLowerCase())
+        //console.log(city)
     }
     //Only state inputted
-    else if(keyword.length===0&&city.length==0&&state.length>0)
-        console.log(state)
+    else if(keyword.length===0&&city.length==0&&state.length>0){
+        formatSearch(state.toLowerCase())
+        //console.log(state)
+    }
     else{
         console.log('need more info!')
     }
+}
+function formatSearch(input){
+    console.log(input.replaceAll(' ','_'))
+    
+}
+function fetchKeyword(keyword){
+    fetch(`https://api.openbrewerydb.org/breweries/search?query=${keyword}`)
+    .then(res=>res.json())
+    .then(brewery=>listBrewery(brewery))
+}
+function fetchCity(city){
+    fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
+    .then(res=>res.json())
+    .then(brewery=>listBrewery(brewery))
+}
+function fetchState(state){
+    fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}`)
+    .then(res=>res.json())
+    .then(brewery=>listBrewery(brewery))
 }
 }
 document.addEventListener("DOMContentLoaded", init);
